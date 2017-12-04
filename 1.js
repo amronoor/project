d6f9b9581f1f839d14140450e27fd12a03c17a25
noor <141047@ppu.edu.ps>
2017-12-04 20:10:18 +0200

    modefing workers.js

--- 1.js
+++ 1.js
@@ -1,46 +1,52 @@
 var ZooKeeper = require ("zookeeper");
 var zk = new ZooKeeper({
-    connect: "localhost:2181"
-    ,timeout: 2000000
-    ,debug_level: ZooKeeper.ZOO_LOG_LEVEL_WARN
-    ,host_order_deterministic: false,
-    data_as_buffer:false
+  connect: "localhost:2181"
+ ,timeout: 2000000
+ ,debug_level: ZooKeeper.ZOO_LOG_LEVEL_WARN
+ ,host_order_deterministic: false,
+ data_as_buffer:false
 });
+zk.connect(function getTasks(err)
 
-zk.connect(function (){
-console.log ("zk session established, id=%s", zk.client_id);
-zk.a_exists("/Tasks",true,function(rc,err,value)
-  {
-    console.log("rc",rc,"err",err,"value",value);
-       
-   });
-
-var tasks_ids;
-
-zk.a_get_children("/Tasks",true,function(rc,err,value){
-    console.log("rc",rc,'err',err,'value',value);
-    console.log("tasks",value);
+{ zk.a_exists("/Tasks",true,function(rc,err,value){
+          console.log("rc",rc,"err",err,"value",value);
 });
+       /// new line
+     
+ //todo: get children (1)
 
-zk.aw_get_children("/Tasks",
-function(type,state,path){
-    console.log("type",type,state,path);
-    zookeeper.a_get_children("/Tasks",true,function(rc,error,value){
-        console.log("tasks",value);
-        });
-},
-function(rc,error,value){
-console.log("tasks",value);
-
-});
-
-
-zk.a_get("/Tasks/child",true,function(rc,err,value,data){
-    console.log("rc",rc,'err',err,'value',value,JSON.stringify(data));
-       data=JSON.parse(data);
-             console.log(data);
+ zk.aw_get_children("/Tasks",
+ function(type,state,path){
+     console.log("type",type,state,path);
+     zk.a_get_children("/Tasks",true,function(rc,error,value){
+         console.log("tasks",value);
+         });
+ },
+ function(rc,error,value){
+ console.log("tasks",value);
+ proccessTasks(value);
  });
+        
+        zk.a_get("/Tasks/child0000013958",false,function(rc,err,value,data){
+            console.log("rc",rc,'err',err,'value',value,JSON.stringify(data));
+           //data=JSON.parse(data);
+             console.log(data);
+             proccessTasks(data);
+             });
 
 
-
+       
+        
 });
+ 
+// proccessTasks(data);
+function proccessTasks(data)
+{
+    var sum=0;
+   
+        sum += data;
+        console.log("Sum=",sum);   
+    
+
+    //updateTotal(sum);
+}
--- workers.js
+++ workers.js
@@ -27,7 +27,7 @@ zk.connect(function getTasks(err)
  proccessTasks(value);
  });
         
-        zk.a_get("/Tasks/child0000012940",false,function(rc,err,value,data){
+        zk.a_get("/Tasks/child0000014959",false,function(rc,err,value,data){
             console.log("rc",rc,'err',err,'value',value,JSON.stringify(data));
            //data=JSON.parse(data);
              console.log(data);
